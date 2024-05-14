62160d85669cc3bf1fc881591349bb63741908d7
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2024-05-14 15:57:08 -0300

    Update Variavel.cs

--- AprendendoC#/Variavel.cs
+++ AprendendoC#/Variavel.cs
@@ -1,10 +1,16 @@
-var name = "Bob";
-int messages = 3;
-decimal temperature = 34.4m;
-Console.Write("Hello, ");
-Console.Write(name);
-Console.Write("! You have ");
-Console.Write(messages);
-Console.Write(" messages in you inbox. The temperature is ");
-Console.Write(temperature);
-Console.Write(" Celsius.");
+internal class Variavel
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Hello, ");
+        string name = "Bob";
+        Console.Write(name);
+        Console.Write("! You have ");
+        int messages = 3;
+        Console.Write(messages);
+        Console.Write(" messages in you inbox. The temperature is ");
+        decimal temperature = 34.4m;
+        Console.Write(temperature);
+        Console.Write(" Celsius.");
+    }
+}
