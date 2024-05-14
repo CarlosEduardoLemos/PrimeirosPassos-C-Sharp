9126998c9d565424b9e6cceb422cb6d9ccbcbf1e
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2024-05-14 14:30:44 -0300

    Initial commit

---
+++ AprendendoC#/Hello.cs
@@ -0,0 +1,13 @@
+// Console.WriteLine("hello Word!");
+
+internal class Hello
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Hello World!");
+
+        Console.Write("Congratulations!");
+        Console.Write(" ");
+        Console.Write("You wrote your first lines of code.");
+    }
+}
