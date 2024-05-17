62a0b6fccca899d255afa6d89c6ec3dfc8c28a4a
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2024-05-17 13:33:17 -0300

    Update

---
+++ retribuicao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+string firstName; // Declarando a variável firstName do tipo string
+
+firstName = "Bob"; // Atribuindo o valor "Bob" à variável firstName
+Console.WriteLine(firstName); // Imprimindo o valor de firstName
+
+firstName = "Liem"; // Atribuindo o valor "Liem" à variável firstName
+Console.WriteLine(firstName); // Imprimindo o valor de firstName
+
+firstName = "Isabella"; // Atribuindo o valor "Isabella" à variável firstName
+Console.WriteLine(firstName); // Imprimindo o valor de firstName
+
+firstName = "Yasmin"; // Atribuindo o valor "Yasmin" à variável firstName
+Console.WriteLine(firstName); // Imprimindo o valor de firstName
