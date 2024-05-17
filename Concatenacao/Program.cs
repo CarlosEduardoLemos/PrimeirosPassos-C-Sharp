88e560e7b6b581a7f789bcead6649edf03382da7
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2024-05-17 19:42:40 -0300

    Update

---
+++ Concatenacao/Program.cs
@@ -0,0 +1,19 @@
+﻿string firstName = "Bob";
+string message = "Hello " + firstName;
+Console.WriteLine(message);
+
+string firstNames = "Bob";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstNames + "!");
+
+int version = 11;
+string updateText = "Update to Windows";
+string message = $"{updateText} {version}";
+Console.WriteLine(message);
+
+int version1 = 11;
+string updateText1 = "Update to Windows";
+Console.WriteLine($"{updateText1} {version1}!");
+
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
