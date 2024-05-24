977b3412ce6649da09d9bd08e8c684cd78e8e80b
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2024-05-23 21:14:31 -0300

    Update

--- DicaComentar/obj/Debug/net8.0/DicaComentar.AssemblyInfo.cs
+++ DicaComentar/obj/Debug/net8.0/DicaComentar.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DicaComentar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44250a186c126ff515a5e4ca36a9e8c2bc6e0555")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b6898f1cc5413a3a0fd14a969491aaf5583ad0")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("DicaComentar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DicaComentar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
