c8ec4ff88ef648e429e62f62ad24ad055dd2500c
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2024-06-06 10:01:58 -0300

    Update

---
+++ MetodoRemove/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program{
+    private static void Main(string[] args)
+    {
+        string message = "This--is--ex-amp-le--da-ta";
+        message = message.Replace("--", " ");
+        message = message.Replace("-", "");
+        Console.Write(message);
+    }
+
+    private string GetDebuggerDisplay()
+    {
+#pragma warning disable CS8603 // Possible null reference return.
+        return ToString();
+#pragma warning restore CS8603 // Possible null reference return.
+    }
+}
