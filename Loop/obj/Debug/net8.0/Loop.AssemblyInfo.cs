0e239098903de8f43c840f8126a511336167af76
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2024-06-09 14:01:40 -0300

    Update

--- Loop/obj/Debug/net8.0/Loop.AssemblyInfo.cs
+++ Loop/obj/Debug/net8.0/Loop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1a56ad667388cf51bef646c7a4d69580f04485a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a79d9fa45534a39505045c11d3d1acc5886be7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MediaAlunos/obj/Debug/net8.0/MediaAlunos.AssemblyInfo.cs
+++ MediaAlunos/obj/Debug/net8.0/MediaAlunos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediaAlunos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1a56ad667388cf51bef646c7a4d69580f04485a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a79d9fa45534a39505045c11d3d1acc5886be7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediaAlunos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediaAlunos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
