0f1439a22ab9c577361013b4b184a052ed124bee
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2024-06-09 15:13:34 -0300

    Update

---
+++ PassagemPorValor/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 3;
+int b = 4;
+int c = 0;
+
+Multiply(a, b, c);
+Console.WriteLine($"global statement: {a} x {b} = {c}");
+
+void Multiply(int a, int b, int c) {
+    c = a * b;
+    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+}
